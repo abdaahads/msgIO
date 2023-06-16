a8719d5abd44c0d608d9fa01b656223a0c0a6662
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-06-16 14:16:33 +0530

    Add files via upload

---
+++ ios/swift/FriendlyChatSwift/FCViewController.swift
@@ -0,0 +1,257 @@
+//
+//  Copyright (c) 2015 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+import Photos
+
+import FirebaseDatabase
+import FirebaseApp
+import FirebaseAuth
+import Firebase.AdMob
+import Firebase.Core
+
+@objc(FCViewController)
+class FCViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,
+    UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+  // Instance variables
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var sendButton: UIButton!
+  var ref: FIRDatabaseReference!
+  var messages: [FIRDataSnapshot]! = []
+  var msglength: NSNumber = 10
+  private var _refHandle: FirebaseHandle!
+
+  var storageRef: FIRStorageReference!
+  var remoteConfig: FIRRemoteConfig!
+
+  @IBOutlet weak var banner: GADBannerView!
+  @IBOutlet weak var clientTable: UITableView!
+
+  @IBAction func didSendMessage(sender: UIButton) {
+    textFieldShouldReturn(textField)
+  }
+
+  @IBAction func didPressCrash(sender: AnyObject) {
+    withVaList([]) {
+      FCRLogv("Cause Crash button clicked", $0)
+    }
+    fatalError()
+  }
+
+  @IBAction func didPressFreshConfig(sender: AnyObject) {
+    fetchConfig()
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    ref = FIRDatabase.database().reference
+
+    remoteConfig = FIRRemoteConfig()
+    // Create Remote Config Setting to enable developer mode.
+    // Fetching configs from the server is normally limited to 5 requests per hour.
+    // Enabling developer mode allows many more requests to be made per hour, so developers
+    // can test different config values during development.
+    let remoteConfigSettings = FIRRemoteConfigSettings()
+    remoteConfigSettings.developerModeEnabled = true
+    remoteConfig.configSettings = remoteConfigSettings
+
+    loadAd()
+    self.clientTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "tableViewCell")
+    fetchConfig()
+    configureStorage()
+
+    // Log that the view did load, FCRNSLogv is used here so the log message will be
+    // shown in the console output. If FCRLogv is used the message is not shown in
+    // the console output.
+    withVaList([]) {
+      FCRNSLogv("View loaded", $0)
+    }
+  }
+
+  func loadAd() {
+    self.banner.adUnitID = FIRContext.sharedInstance().adUnitIDForBannerTest
+    self.banner.rootViewController = self
+    self.banner.loadRequest(GADRequest())
+  }
+
+  func fetchConfig() {
+    var expirationDuration: Double = 3600
+    // If in developer mode cacheExpiration is set to 0 so each fetch will retrieve values from
+    // the server.
+    if (self.remoteConfig.configSettings.developerModeEnabled) {
+      expirationDuration = 0
+    }
+
+    // cacheExpirationSeconds is set to cacheExpiration here, indicating that any previously
+    // fetched and cached config would be considered expired because it would have been fetched
+    // more than cacheExpiration seconds ago. Thus the next fetch would go to the server unless
+    // throttling is in progress. The default expiration duration is 43200 (12 hours).
+    remoteConfig.fetchWithExpirationDuration(expirationDuration) { (status, error) in
+      if (status == .Success) {
+        print("Config fetched!")
+        self.remoteConfig.activateFetched()
+        self.msglength = self.remoteConfig["friendly_msg_length"].numberValue!
+        print("Friendly msg length config: \(self.msglength)")
+      } else {
+        print("Config not fetched")
+        print("Error \(error)")
+      }
+    }
+  }
+
+  func configureStorage() {
+    let app = FIRFirebaseApp.app()
+    storageRef = FIRStorage.storage(app: app!).reference
+  }
+
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    guard let text = textField.text else { return true }
+
+    let newLength = text.utf16.count + string.utf16.count - range.length
+    return newLength <= self.msglength.integerValue // Bool
+  }
+
+  override func viewWillAppear(animated: Bool) {
+    self.messages.removeAll()
+    // Listen for new messages in the Firebase database
+    _refHandle = self.ref.childByAppendingPath("messages").observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
+      self.messages.append(snapshot)
+      self.clientTable.insertRowsAtIndexPaths([NSIndexPath(forRow: self.messages.count-1, inSection: 0)], withRowAnimation: .Automatic)
+    })
+  }
+
+  override func viewWillDisappear(animated: Bool) {
+    self.ref.removeObserverWithHandle(_refHandle)
+  }
+
+  // UITableViewDataSource protocol methods
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return messages.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    // Dequeue cell
+    let cell: UITableViewCell! = self.clientTable .dequeueReusableCellWithIdentifier("tableViewCell", forIndexPath: indexPath)
+    // Unpack message from Firebase DataSnapshot
+    let messageSnapshot: FIRDataSnapshot! = self.messages[indexPath.row]
+    let message = messageSnapshot.value as! Dictionary<String, String>
+    let name = message[Constants.MessageFields.name] as String!
+    if let imageUrl = message[Constants.MessageFields.imageUrl] {
+      if imageUrl.hasPrefix("gs://") {
+        FIRStorage.storage(app: FIRFirebaseApp.app()!).referenceForURL(imageUrl).dataWithCompletion(){ (data, error) in
+          if let error = error {
+            print("Error downloading: \(error)")
+            return
+          }
+          cell.imageView?.image = UIImage.init(data: data!)
+        }
+      } else if let url = NSURL(string:imageUrl), data = NSData(contentsOfURL: url) {
+        cell.imageView?.image = UIImage.init(data: data)
+      }
+      cell!.textLabel?.text = "sent by: \(name)"
+    } else {
+      let text = message[Constants.MessageFields.text] as String!
+      cell!.textLabel?.text = name + ": " + text
+      cell!.imageView?.image = UIImage(named: "ic_account_circle")
+      if let photoUrl = message[Constants.MessageFields.photoUrl], url = NSURL(string:photoUrl), data = NSData(contentsOfURL: url) {
+        cell!.imageView?.image = UIImage(data: data)
+      }
+    }
+    return cell!
+  }
+
+  // UITextViewDelegate protocol methods
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    let data = [Constants.MessageFields.text: textField.text! as String]
+    sendMessage(data)
+    return true
+  }
+
+  func sendMessage(data: [String: String]) {
+    var mdata = data
+    mdata[Constants.MessageFields.name] = AppState.sharedInstance.displayName
+    if let photoUrl = AppState.sharedInstance.photoUrl {
+      mdata[Constants.MessageFields.photoUrl] = photoUrl.absoluteString
+    }
+    // Push data to Firebase Database
+    self.ref.childByAppendingPath("messages").childByAutoId().setValue(mdata)
+  }
+
+  // MARK: - Image Picker
+
+  @IBAction func didTapAddPhoto(sender: AnyObject) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+      picker.sourceType = .Camera
+    } else {
+      picker.sourceType = .PhotoLibrary
+    }
+
+    presentViewController(picker, animated: true, completion:nil)
+  }
+
+  func imagePickerController(picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+      picker.dismissViewControllerAnimated(true, completion:nil)
+
+      let referenceUrl = info[UIImagePickerControllerReferenceURL] as! NSURL
+      let assets = PHAsset.fetchAssetsWithALAssetURLs([referenceUrl], options: nil)
+      let asset = assets.firstObject
+      asset?.requestContentEditingInputWithOptions(nil, completionHandler: { (contentEditingInput, info) in
+        let imageFile = contentEditingInput?.fullSizeImageURL?.absoluteString
+        let filePath = "\(FIRAuth.auth()?.currentUser?.uid)/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000))/\(referenceUrl.lastPathComponent!)"
+        let metadata = FIRStorageMetadata()
+        metadata.contentType = "image/jpeg"
+        self.storageRef.childByAppendingPath(filePath)
+          .putFile(imageFile!, metadata: metadata) { (metadata, error) in
+            if let error = error {
+              print("Error uploading: \(error.description)")
+              return
+            }
+            self.sendMessage([Constants.MessageFields.imageUrl: self.storageRef.childByAppendingPath((metadata?.path)!).description])
+          }
+      })
+  }
+
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    picker.dismissViewControllerAnimated(true, completion:nil)
+  }
+
+  @IBAction func signOut(sender: UIButton) {
+    let firebaseAuth = FIRAuth.auth()
+    do {
+      try firebaseAuth?.signOut()
+      AppState.sharedInstance.signedIn = false
+      performSegueWithIdentifier(Constants.Segues.FpToSignIn, sender: nil)
+    } catch let signOutError as NSError {
+      print ("Error signing out: \(signOutError)")
+    }
+  }
+
+  func showAlert(title:String, message:String) {
+    dispatch_async(dispatch_get_main_queue()) {
+        let alert = UIAlertController(title: title,
+            message: message, preferredStyle: .Alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .Destructive, handler: nil)
+        alert.addAction(dismissAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+  }
+
+}
